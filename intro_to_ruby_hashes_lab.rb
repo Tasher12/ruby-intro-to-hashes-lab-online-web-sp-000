90d2f05bcc2e00c0ae33cd42f34de14dbc14e2f6
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-20 16:26:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,10 +53,10 @@ def monopoly_with_fourth_tier
 	    :pieces => 4,
 	    :names => {
 	      :reading_railroad => {
-	        :mortage_value => '$100'
+	        :mortage_value => "$100"
 	      },
 	      :pennsylvania_railroad => {
-	        :mortage_value => '$200'
+	        :mortage_value => "$200"
 	      },
 	      :b_and_o_railroad => {
 	        :mortage_value => "$400"
